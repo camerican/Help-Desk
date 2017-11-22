1846c75d448269be19746ca1f66efad18b87e388
Cam Crews <camcrews@gmail.com>
2017-11-22 12:43:39 -0500

    Error checking on cocktail data

--- demos/cocktaildb/app.rb
+++ demos/cocktaildb/app.rb
@@ -6,8 +6,14 @@ get '/' do
   erb :index
 end
 post '/' do
-  @results = JSON.parse(HTTParty.get("http://www.thecocktaildb.com/api/json/v1/1/filter.php?i=#{params[:ingredient]}").body)["drinks"]
+  response = HTTParty.get("http://www.thecocktaildb.com/api/json/v1/1/filter.php?i=#{params[:ingredient]}")
+  if response && response.body 
+    @results = JSON.parse(response.body)["drinks"]
+  else
+    @results = nil
+  end
   # the results are oddly formatted, so we have to access an array of 
   # results via a "drinks" key on the object that is returned
   erb :index
 end
+
